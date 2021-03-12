4d37e85060647e309a656adf68ff2ef0fed39215
Derek Todd <derekhtodd@yahoo.co.uk>
2021-03-12 16:25:47 +0000

    Started work

---
+++ src/DisplayProjectedValues.js
@@ -0,0 +1,7 @@
+export default function DisplayProjectedValues ({ title="Projected values", retirementAge, policyValue}) {
+    return (
+        <section>
+            <h1>{title}</h1>
+        </section>
+    )
+};
---
+++ src/PensionParametersForm.js
@@ -0,0 +1,32 @@
+import { useInput } from "./hooks";
+
+export default function PensionParametersForm ({ onNewParameters = f => f}) {
+    const [retirementAge, resetRetirementAge] = useInput(67);
+    const [policyValue, resetPolicyValue] = useInput(0);
+
+    const submit = event => {
+        event.preventDefault();
+        onNewParameters(retirementAge.value, policyValue.value);
+        //resetRetirementAge();
+        //resetPolicyValue();
+        // Use these if implementing an 'add pension' option.
+    };
+
+    return ( 
+        <form onSubmit={submit}>
+            <input
+            {...retirementAge}
+            type="text"
+            placeholder="Retirement Age (e.g. 67)"
+            required
+            />
+            <input
+            {...policyValue}
+            type="text"
+            placeholder="Value of your policy."
+            required
+            />
+            <button>Calculate</button>
+        </form>    
+    );
+};
