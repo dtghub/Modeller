cd3e000425fb2602cf34b08bcd2f1e6e1e787b62
Derek Todd <derekhtodd@yahoo.co.uk>
2021-06-05 21:56:51 +0100

    Refactoring to a better interface

--- src/Pension.js
+++ src/Pension.js
@@ -1,3 +1,4 @@
+import { useInput } from "./hooks";
 import CalculateProjectedValue from "./CalculateProjectedValue";
 import { FaTrash } from "react-icons/fa";
 import { usePensions } from "./PensionProvider";
@@ -5,10 +6,18 @@ import { usePensions } from "./PensionProvider";
 export default function Pension({ id, provider, policy, retirementAge, pensionValue }) {
   const { removePension } = usePensions();
   const myParams = {retirementAge, pensionValue};
+  const [providerName, resetProviderName] = useInput(provider);
   return (
     <section>
       <div style={{border:"1px solid red"}}>
-        <h1>Provider: {provider}</h1>
+        <form>
+          <input
+          {...providerName}
+          type="text"
+          placeholder="Pension company (e.g. Scottish Widows)"
+          required
+          />
+        </form> 
         <h2>Policy number: {policy}</h2>
         <div>Retirement Age: {retirementAge}</div>
         <div>Pension value: {pensionValue}</div>
